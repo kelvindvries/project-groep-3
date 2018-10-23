4f4fb192a51ea76536cfdb9140c9d9c06584a912
Kelvin de Vries <Kelvin.devries@gmail.com>
2018-10-23 13:15:36 +0200

    Get_api en thuisbioscoop.xml

---
+++ GUI.py
@@ -0,0 +1,18 @@
+from user_login import *
+from tkinter import *
+
+def signupframe():
+    #code voor signup
+
+def loginframe():
+    # code voor inloggen, moet dus login bekijken of het even is met sign up waardes
+
+def overzicht():
+    #code van overzicht film
+
+
+root = Tk()
+
+
+
+root.mainloop()
