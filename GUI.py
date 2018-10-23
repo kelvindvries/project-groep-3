40dab9893f739a2bc50c1570295033428ff09cae
Steen3S <3s@steenberghe.com>
2018-10-23 15:35:05 +0200

    werkende api class

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 from user_login import *
 from tkinter import *
-from classes import get_api
+from classes import api
 
 
 
@@ -16,18 +16,18 @@ titel = Label(master=root,
               height=5)
 
 titels = Label(master=root,
-              text=get_api.tkinter_data.read(),
-              background='yellow',
-              foreground='blue',
-              font=('Helvetica', 8, 'bold italic'),
-              width=100,
-              height=50)
+               text=api.tkinter_data.read(),
+               background='yellow',
+               foreground='blue',
+               font=('Helvetica', 8, 'bold italic'),
+               width=100,
+               height=50)
 
 
 titel.pack()
 titels.pack()
 
-get_api.tkinter_data.close()
+api.tkinter_data.close()
 
 
 
